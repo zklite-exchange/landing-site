44f4608ce095763ac9c81002ddd5115c28f73f1d
Khang NT <vn.khangnt@gmail.com>
2024-05-06 21:20:29 +0700

    add wide banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -105,7 +105,11 @@ const Homepage: NextPage = () => {
     >
       <Head>
         <title>zkLite Exchange</title>
-        <meta name="description" content="zkLite Exchange website" />
+        <meta name="description" content="An order-book DEX on zkSync Lite" />
+        <meta property="og:title" content="zkLite Exchange" />
+        <meta property="og:description" content="An order-book DEX on zkSync Lite" />
+        <meta property="og:image" content="/assets/wide_banner.png" />
+        
         <link rel="apple-touch-icon" sizes="57x57" href="/icons/apple-icon-57x57.png" />
         <link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-icon-60x60.png" />
         <link rel="apple-touch-icon" sizes="72x72" href="/icons/apple-icon-72x72.png" />
